c7bf00e9cb5e1e8fa83576c2a3f8dd5984b8dfd3
sagar-ss <sagarsrivastava0302@gmail.com>
2023-12-04 12:27:36 +0530

    setting unsplash url

--- client/src/SearchForm.jsx
+++ client/src/SearchForm.jsx
@@ -1,6 +1,22 @@
 const SearchForm = () => {
+
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    const searchValue = e.target.elements.search.value;
+    // console.log(e.target.elements)
+    // console.log(e.target.elements.search)
+    if(!searchValue)return
+    console.log(searchValue)
+
+  }
   return (
-    <h2>Search Form</h2>
+    <section>
+      <h1 className="title">unsplash images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input type="text" placeholder="cat" name="search" className="form-input search-input"/>
+        <button type="submit" className="btn">Search</button>
+      </form>
+    </section>
   )
 }
 
